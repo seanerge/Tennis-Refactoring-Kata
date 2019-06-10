187765844af8c174728f3342ff1b959fd19c2af6
sc3l <sean.z.chen@newegg.com>
2019-06-10 19:19:37 +0800

    抽出 1. 平局, 2. 局點, 3. 其他 method

---
+++ Tennis-Refactoring-Kata/Tennis/ExampleGameTennisTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tennis;
+
+namespace Tennis_Refactoring_Kata.Tennis
+{
+    [TestFixture]
+
+    public class ExampleGameTennisTest
+    {
+        [Test]
+        public void CheckGame1()
+        {
+            var game = new TennisGame1("player1", "player2");
+            RealisticTennisGame(game);
+        }
+
+        ////[Test]
+        ////public void CheckGame2()
+        ////{
+        ////    var game = new TennisGame2("player1", "player2");
+        ////    RealisticTennisGame(game);
+        ////}
+
+        ////[Test]
+        ////public void CheckGame3()
+        ////{
+        ////    var game = new TennisGame3("player1", "player2");
+        ////    RealisticTennisGame(game);
+        ////}
+
+        private void RealisticTennisGame(ITennisGame game)
+        {
+            string[] points = { "player1", "player1", "player2", "player2", "player1", "player1" };
+            string[] expectedScores = { "Fifteen-Love", "Thirty-Love", "Thirty-Fifteen", "Thirty-All", "Forty-Thirty", "Win for player1" };
+            for (var i = 0; i < 6; i++)
+            {
+                game.WonPoint(points[i]);
+                Assert.AreEqual(expectedScores[i], game.GetScore());
+            }
+        }
+    }
+}
--- Tennis-Refactoring-Kata/Tennis/TennisGame1.cs
+++ Tennis-Refactoring-Kata/Tennis/TennisGame1.cs
@@ -23,58 +23,80 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                return GetEqualScore();
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                return GetGamePointScore();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
+                return GetOtherScore();
+            }
+        }
+
+        private string GetGamePointScore()
+        {
+            string score = "";
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+
+            return score;
+        }
+
+        private string GetEqualScore()
+        {
+            string score = "";
+            switch (m_score1)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+
+            }
+
+            return score;
+        }
+
+        private string GetOtherScore()
+        {
+            var score = "";
+            int tempScore = 0;
+            for (var i = 1; i < 3; i++)
+            {
+                if (i == 1) tempScore = m_score1;
+                else { score += "-"; tempScore = m_score2; }
+                switch (tempScore)
                 {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
                 }
             }
+
             return score;
         }
     }
