1a6fbea3635328407500789595646c89e60f9aed
sc3l <sean.z.chen@newegg.com>
2019-06-24 19:36:19 +0800

    refactor: get normal game score

--- Tennis-Refactoring-Kata/Tennis/TennisGame2.cs
+++ Tennis-Refactoring-Kata/Tennis/TennisGame2.cs
@@ -30,40 +30,20 @@
 
             if (p1point > 0 && p2point == 0)
             {
-                p1res = GetScoreName(p1point);
-                p2res = "Love";
-                score = p1res + "-" + p2res;
+                score = GetNormalGameScore();
             }
             if (p2point > 0 && p1point == 0)
             {
-                p2res = GetScoreName(p2point);
-                p1res = "Love";
-                score = p1res + "-" + p2res;
+                score = GetNormalGameScore();
             }
 
             if (p1point > p2point && p1point < 4)
             {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
+                score = GetNormalGameScore();
             }
             if (p2point > p1point && p2point < 4)
             {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
+                score = GetNormalGameScore();
             }
 
             if (p1point > p2point && p2point >= 3)
@@ -84,6 +64,15 @@
             {
                 score = "Win for player2";
             }
+            return score;
+        }
+
+        private string GetNormalGameScore()
+        {
+            string score;
+            p1res = GetScoreName(p1point);
+            p2res = GetScoreName(p2point);
+            score = p1res + "-" + p2res;
             return score;
         }
 
