1b9ef8dff9b5ef069984c08b8955d1f54a68f134
sc3l <sean.z.chen@newegg.com>
2019-07-15 19:43:13 +0800

    refactor: inline method

--- Tennis-Refactoring-Kata/Tennis/TennisGame2.cs
+++ Tennis-Refactoring-Kata/Tennis/TennisGame2.cs
@@ -1,18 +1,15 @@
-﻿namespace Tennis
+﻿using System;
+
+namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
-
-        private string player1Name;
-        private string player2Name;
+        Player p1, p2;
 
         public TennisGame2(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            p1point = 0;
-            this.player2Name = player2Name;
+            p1 = new Player(player1Name, 0);
+            p2 = new Player(player2Name, 0);
         }
 
         public string GetScore()
@@ -23,84 +20,61 @@
             {
                 score = GetDrawScore();
             }
-
-            if (IsNormalCase())
+            else
             {
-                score = GetNormalGameScore();
-            }
-
-            if (IsP1Advantage())
-            {
-                score = "Advantage player1";
-            }
-
-            if (IsP2Advantage())
-            {
-                score = "Advantage player2";
-            }
-
-            if (IsP1Winner() || IsP2Winner())
-            {
-                var winner = string.Empty;
-
-                if (IsP1Winner())
+                if ((p1.Point > 0 && p2.Point == 0)
+                    || (p2.Point > 0 && p1.Point == 0)
+                    || (p1.Point > p2.Point && p1.Point < 4)
+                    || (p2.Point > p1.Point && p2.Point < 4))
                 {
-                    winner = "player1";
+                    score = GetNormalGameScore();
                 }
 
-                if (IsP2Winner())
+                if (p1.Point > p2.Point)
                 {
-                    winner = "player2";
+                    if (p2.Point >= 3)
+                    {
+                        score = "Advantage player1";
+                    }
+
+                    if (p1.Point >= 4 && Math.Abs(p1.Point - p2.Point) >= 2)
+                    {
+                        score = $"Win for {p1.Name}";
+                    }
                 }
 
-                score = $"Win for {winner}";
+                if (p2.Point > p1.Point)
+                {
+                    if (p1.Point >= 3)
+                    {
+                        score = "Advantage player2";
+                    }
+
+                    if (p2.Point >= 4 && Math.Abs(p2.Point - p1.Point) >= 2)
+                    {
+                        score = $"Win for {p2.Name}";
+                    }
+                } 
             }
+
             return score;
         }
 
         private bool IsDrawCase()
         {
-            return p1point == p2point;
-        }
-
-        private bool IsNormalCase()
-        {
-            return (p1point > 0 && p2point == 0)
-                            || (p2point > 0 && p1point == 0)
-                            || (p1point > p2point && p1point < 4)
-                            || (p2point > p1point && p2point < 4);
-        }
-
-        private bool IsP2Advantage()
-        {
-            return p2point > p1point && p1point >= 3;
-        }
-
-        private bool IsP1Advantage()
-        {
-            return p1point > p2point && p2point >= 3;
-        }
-
-        private bool IsP2Winner()
-        {
-            return p2point >= 4 && (p2point - p1point) >= 2;
-        }
-
-        private bool IsP1Winner()
-        {
-            return p1point >= 4 && (p1point - p2point) >= 2;
+            return p1.Point == p2.Point;
         }
 
         private string GetDrawScore()
         {
             var score = string.Empty;
 
-            if (p1point < 3)
+            if (p1.Point < 3)
             {
-                score = GetScoreName(p1point);
+                score = GetScoreName(p1.Point);
                 score += "-All";
             }
-            if (p1point > 2)
+            if (p1.Point > 2)
                 score = "Deuce";
 
             return score;
@@ -108,7 +82,7 @@
 
         private string GetNormalGameScore()
         {
-            return GetScoreName(p1point) + "-" + GetScoreName(p2point);
+            return GetScoreName(p1.Point) + "-" + GetScoreName(p2.Point);
         }
 
         private string GetScoreName(int point)
@@ -128,12 +102,12 @@
 
         private void P1Score()
         {
-            p1point++;
+            p1.Point++;
         }
 
         private void P2Score()
         {
-            p2point++;
+            p2.Point++;
         }
 
         public void WonPoint(string player)
@@ -143,6 +117,20 @@
             else
                 P2Score();
         }
+
+    }
+
+    public class Player
+    {
+        public Player(string name, int point)
+        {
+            this.Name = name;
+            this.Point = point;
+        }
+
+        public int Point { get; set; }
+        public string Name { get; set; }
+
 
     }
 }
